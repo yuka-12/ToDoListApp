28d2e7835906569a86313b5eec4b92cbf5655136
yuka <ykmrykmrykmrykmr@gmail.com>
2020-10-26 12:39:03 +0100

    Modified according to Redux optimization

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,80 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import ToDoForm from './ToDoForm.js';
-import ToDoList from './ToDoList.js';
+import { PrivateRoute } from './_components';
+import { Login } from './Login.js';
+import { RegisterPage } from './RegisterPage/registerPage';
+import { HomePage } from './HomePage.js';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	NavLink,
+	Redirect,
+} from 'react-router-dom';
+import { history } from './_helpers';
+import { alertActions } from './_actions';
+import { connect } from 'react-redux';
 
-const App = () => {
-	const [error] = useState(null);
-	const [items, setItems] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-	useEffect(() => {
-		(async () => {
-			setIsLoading(true)
-			const res = await getTodos().catch(err => console.log(err));
-			if (res) {
-				setItems(res.item)
-				setIsLoading(false)
-			}
-		})()
-	}, [])
-
-	const getTodos = async () => {
-		const response = await fetch('/api/get.php');
-		const data = await response.json();
-		if (response.status !== 200) throw Error(data.message);
-		setItems(data.item)
-		return data
-	};
-
-	if (error) {
-			return <div> {error.message} </div>;
-	} else if (isLoading) {
-		return <div> Loading... </div>;
+		history.listen((location, action) => {
+			// clear alert on location change
+			this.props.clearAlerts();
+		});
 	}
-	
-	return (
+	render() {
+		const { alert } = this.props;
+
+		return (
+			<Router>
 				<div className='App'>
+					<div>
+						{alert.message && (
+							<div className={`alert ${alert.type}`}>{alert.message}</div>
+						)}
+					</div>
 					<Navbar bg='light' sticky='top' className='justify-content-between'>
-						<a className='navbar-brand' href='/'>
+						<NavLink className='navbar-brand' to='/'>
 							<h1> ToDoListApp </h1>
-						</a>
+						</NavLink>
 						<Nav>
-							<Nav.Link href='/' className='ml-10'>
+							<NavLink to='/login' className='ml-10'>
 								<Button variant='secondary'> Login </Button>
-							</Nav.Link>
+							</NavLink>
 						</Nav>
 					</Navbar>
 					<Container fluid>
-						<Row>
-							<Col md='3' lg='3'>
-								<p className='ml-2 mt-4'> Friends list </p>
-							</Col>
-							<Col md='9' lg='9'>
-								<div className='ml-2 mt-4'>
-									<h2 className='mb-4'> Your ToDoList </h2>
-									<hr></hr>
-									<ToDoForm />
-									<div className='mt-3 d-flex flex-column'>
-										<ul>
-										{items.map(item => (
-											<ToDoList item={item} />
-										))}
-										</ul>
-									</div>
-								</div>
-							</Col>
-						</Row>
+						<Switch>
+							<PrivateRoute exact path='/' component={HomePage} />
+							<Route path='/login' component={Login} />
+							<Route path='/createAccount' component={RegisterPage} />
+							<Redirect from='*' to='/' />
+						</Switch>
 					</Container>
 				</div>
-			);
-		}
+			</Router>
+		);
+	}
+}
 
+function mapState(state) {
+	const { alert } = state;
+	return { alert };
+}
 
-export default App;
+const actionCreators = {
+	clearAlerts: alertActions.clear,
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
