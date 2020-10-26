a526bd6476ed6a1695be76c5544e407767eec604
yuka <ykmrykmrykmrykmr@gmail.com>
2020-10-26 12:21:22 +0100

    Created HomePage component

---
+++ my-app/src/HomePage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ToDoForm from './ToDoForm.js';
+import ToDoList from './ToDoList.js';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/Col';
+
+import { userActions } from './_actions';
+
+class HomePage extends React.Component {
+	componentDidMount() {
+		this.props.getUsers();
+	}
+
+	render() {
+		const { user, users } = this.props;
+		console.log(users);
+		return (
+			<Row>
+				<Col md='3' lg='3'>
+					<p className='ml-2 mt-4'> Friends list </p>
+				</Col>
+				<Col md='9' lg='9'>
+					<div className='ml-2 mt-4'>
+						<h2 className='mb-4'> Your ToDoList </h2>
+						<hr></hr>
+						<ToDoForm />
+						<div className='mt-3 d-flex flex-column'>
+							<ul>
+								{users.items &&
+									users.items.item.map((item) => <ToDoList item={item} />)}
+							</ul>
+						</div>
+					</div>
+				</Col>
+			</Row>
+		);
+	}
+}
+
+function mapState(state) {
+	const { users, authentication } = state;
+	const { user } = authentication;
+	return { user, users };
+}
+
+const actionCreators = {
+	getUsers: userActions.getAll,
+};
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as HomePage };
+// export default HomePage;
