ad23cd1708609c3e8da180b03d05e7c69f57d528
yuka <ykmrykmrykmrykmr@gmail.com>
2020-10-30 11:56:42 +0100

    Changed the database connection section to reference the database information from environment variables

--- api/api/Database.class.php
+++ api/api/Database.class.php
@@ -7,9 +7,20 @@ class Database {
   protected $table;
   protected $columns;
 
-  public function __construct($servername = "ToDoList_mysql", $username = "dbuser", $password = "password") {
+  private $servername;
+  private $dbname;
+  private $username;
+  private $password;
+  
+
+  public function __construct() {
+    $this->servername = getenv('DB_SERVER_NAME');
+    $this->dbname = getenv('DB_NAME');
+    $this->username = getenv('DB_USER_NAME');
+    $this->password = getenv('DB_PASSWORD');
+  
     try {
-      $this->dbh = new PDO("mysql:host=$servername;dbname=ToDoList", $username, $password);
+      $this->dbh = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
       $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
       throw new Exception($e->getMessage());
